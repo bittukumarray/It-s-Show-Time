37d6c33d17b57ba8b7145d22eaa0c0e15fe4b0bb
bittukumarray <raybittu242@gmail.com>
2019-03-26 10:53:19 +0530

    some bug fixes

--- moviebooking/user/views.py
+++ moviebooking/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import UserProfile, Rating, Movie, Comment
+from django.contrib.auth.models import User
 
 # Create your views here.
+def Index(request):
+    movie = Movie.objects.all()
+    rating = Rating.objects.all()
+    comment = Comment.objects.all()
+    userprofile = UserProfile.objects.all()
+
+    return render(request, 'user/index.html', {'movie': movie, 'rating': rating, 'comment': Comment, 'userprofile': UserProfile})
